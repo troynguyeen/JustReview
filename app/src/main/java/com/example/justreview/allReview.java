bfb579831448f12b306459c047b89b8c80b4f4af
khoi1 <truongbaokhoi2252001@gmail.com>
2021-09-27 06:31:54 +0700

    layout tất cả bài viết

---
+++ app/src/main/java/com/example/justreview/allReview.java
@@ -0,0 +1,21 @@
+package com.example.justreview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class allReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_review);
+
+
+    }
+
+
+
+}
