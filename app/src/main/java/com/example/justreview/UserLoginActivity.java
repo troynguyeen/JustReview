5ff3e59176c8e63a5ea36b23d4562aaad5bd3a92
saladassvn <buiquocdat321@gmail.com>
2021-10-15 15:03:57 +0700

    no

--- app/src/main/java/com/example/justreview/UserLoginActivity.java
+++ app/src/main/java/com/example/justreview/UserLoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.justreview;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class UserLoginActivity extends MainActivity{
 
@@ -8,7 +12,6 @@ public class UserLoginActivity extends MainActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
 
     }
 }
