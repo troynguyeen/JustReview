b31be5a62e27713f403f848d6db97953168f410d
lyquochoa <lyquochoa000@gmail.com>
2021-11-17 10:30:35 +0700

    Fix code

--- app/src/main/java/com/example/justreview/MainActivity.java
+++ app/src/main/java/com/example/justreview/MainActivity.java
@@ -65,7 +65,6 @@ public class MainActivity extends AppCompatActivity implements
     TextView textViewAll;
     private SharedPreferenceConfig sharedPreferenceConfig;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
